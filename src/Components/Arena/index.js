2ef14a5595a9c663369e0dcd5d610807fd512f41
Kevin Kuang <200sxmeda@gmail.com>
2021-10-31 13:14:40 +1100

    Render Character

--- src/Components/Arena/index.js
+++ src/Components/Arena/index.js
@@ -45,7 +45,6 @@ const Arena = ({ characterNFT }) => {
 
     return (
         <div className="arena-container">
-            {/* Replace your Boss UI with this */}
             {boss && (
                 <div className="boss-container">
                     <div className={`boss-content`}>
@@ -65,9 +64,29 @@ const Arena = ({ characterNFT }) => {
                     </div>
                 </div>
             )}
-
-            {/* Character NFT */}
-            <p>CHARACTER NFT GOES HERE</p>
+            {characterNFT && (
+                <div className="players-container">
+                    <div className="player-container">
+                        <h2>Your Character</h2>
+                        <div className="player">
+                            <div className="image-content">
+                                <h2>{characterNFT.name}</h2>
+                                <img
+                                    src={characterNFT.imageURI}
+                                    alt={`Character ${characterNFT.name}`}
+                                />
+                                <div className="health-bar">
+                                    <progress value={characterNFT.hp} max={characterNFT.maxHp} />
+                                    <p>{`${characterNFT.hp} / ${characterNFT.maxHp} HP`}</p>
+                                </div>
+                            </div>
+                            <div className="stats">
+                                <h4>{`⚔️ Attack Damage: ${characterNFT.attackDamage}`}</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
