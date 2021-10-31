c9c92b1bf7b80877a7935c92551beedc134fde7c
Kevin Kuang <200sxmeda@gmail.com>
2021-10-31 13:51:38 +1100

    Add loading indicator while connecting wallet

--- src/App.js
+++ src/App.js
@@ -4,38 +4,38 @@ import myEpicGame from './utils/MyEpicGame.json';
 import './App.css';
 import SelectCharacter from './Components/SelectCharacter';
 import Arena from './Components/Arena';
+import LoadingIndicator from './Components/LoadingIndicator';
 import twitterLogo from './assets/twitter-logo.svg';
 import { ethers } from 'ethers';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'wagmi_wagmi';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   // State
   const [currentAccount, setCurrentAccount] = useState(null);
   const [characterNFT, setCharacterNFT] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Actions
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
-
       if (!ethereum) {
         console.log('Make sure you have MetaMask!');
         return;
       } else {
         console.log('We have the ethereum object', ethereum);
       }
-
       const accounts = await ethereum.request({ method: 'eth_accounts' });
-
       if (accounts.length !== 0) {
         const account = accounts[0];
         console.log('Found an authorized account:', account);
         setCurrentAccount(account);
       } else {
         console.log('No authorized account found');
+        setIsLoading(false);
       }
     } catch (error) {
       console.log(error);
@@ -43,6 +43,7 @@ const App = () => {
   };
 
   const connectWalletAction = async () => {
+    setIsLoading(true);
     try {
       const { ethereum } = window;
       if (!ethereum) {
@@ -80,6 +81,7 @@ const App = () => {
       } else {
         console.log('No character NFT found');
       }
+      setIsLoading(false);
     };
     if (currentAccount) {
       console.log('CurrentAccount:', currentAccount);
@@ -89,6 +91,9 @@ const App = () => {
 
   // Render Methods
   const renderContent = () => {
+    if (isLoading) {
+      return <LoadingIndicator />;
+    }
     if (!currentAccount) {
       return (
         <div className="connect-wallet-container">
@@ -126,7 +131,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built with @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE} with @_buildspace`}</a>
         </div>
       </div>
     </div>
