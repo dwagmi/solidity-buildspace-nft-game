b7c5f4643b0299b51890dbc5721b90d08ee557d4
Kevin Kuang <200sxmeda@gmail.com>
2021-10-31 12:44:18 +1100

    Add contract to react state

--- src/Components/SelectCharacter/index.js
+++ src/Components/SelectCharacter/index.js
@@ -1,10 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import './SelectCharacter.css';
+import { ethers } from 'ethers';
+import { CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
+import myEpicGame from '../../utils/MyEpicGame.json';
 
 /*
  * Don't worry about setCharacterNFT just yet, we will talk about it soon!
  */
 const SelectCharacter = ({ setCharacterNFT }) => {
+    const [characters, setCharacters] = useState([]);
+    const [gameContract, setGameContract] = useState(null);
+
+    useEffect(() => {
+        const { ethereum } = window;
+        if (ethereum) {
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            const gameContract = new ethers.Contract(
+                CONTRACT_ADDRESS,
+                myEpicGame.abi,
+                signer
+            );
+            setGameContract(gameContract);
+        } else {
+            console.log('Ethereum object not found');
+        }
+    }, []);
     return (
         <div className="select-character-container">
             <h2>Mint Your Hero. Choose wisely.</h2>
